01f123e2f6eb20a323a99a8f2e6b1dbf1a5b0657
JoydipShome <theshome@gmail.com>
2020-07-23 20:21:27 +0530

    First Commit

---
+++ Django3-Password-Generator/generator/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+    # return HttpResponse('Hello There')
+
+def about(request):
+    return render(request, 'generator/about.html')
+    # return HttpResponse('Hello There')
+
+def password(request):
+    
+    characters = list('abcdqwertyuioplkjhgfdsazxcvbnm')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ASDFGHKLIUYTREWQXXVB'))
+
+    if request.GET.get('special'):
+        characters.extend(list('%$&^@#$%^&%%'))
+    
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length',12))
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+
+    return render(request, 'generator/password.html',{'password': thepassword})
+   
+
